d2adad1165fab49f718f4117d7bf3a163f2ff26c
Mak-Pro <maksprocode@gmail.com>
2020-08-08 15:54:26 +0300

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+// 1.Импортируем модуль
+import PropTypes from 'prop-types';
 
 
 class App extends Component {
@@ -13,13 +15,31 @@ class App extends Component {
 
 	render() {
 
+		const { apptitle, appcounter } = this.props;
+
 		return (
 			<div className="content__container">
-				Content
+				<h1>{apptitle}</h1>
+				<span>{appcounter}</span>
 			</div>
 	  );
 	}
-  
+}
+
+/*
+	2. Добавляем проверку:
+
+	optionalArray: PropTypes.array,
+	optionalBool: PropTypes.bool,
+	optionalFunc: PropTypes.func,
+	optionalNumber: PropTypes.number,
+	optionalObject: PropTypes.object,
+	optionalString: PropTypes.string,
+	optionalSymbol: PropTypes.symbol,
+*/
+App.propTypes = {
+	apptitle: PropTypes.string,
+	appcounter: PropTypes.number
 }
 
 export default App;
