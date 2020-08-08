b141e441cbd5c768914a28ca5197bccfb0b73ff1
Mak-Pro <maksprocode@gmail.com>
2020-08-08 16:21:08 +0300

    refs

--- src/App.js
+++ src/App.js
@@ -5,17 +5,46 @@ import React, { Component } from 'react';
 class App extends Component {
 	constructor(props) {
 		super(props);
+		// 2) Второй способ - современный
+		this.inputElementRef = React.createRef();
 	}
 
 	state = {
-		
+		pagename: ''
+	}
+
+	componentDidMount() {
+		// Первый способ - устаревший
+		// this.inputElement.focus();
+
+		// 3) Второй способ - современный
+		this.inputElementRef.current.focus();
+	}
+
+	changeTextHandler = (e) => {
+		this.setState({
+			pagename: e.target.value
+		});
 	}
 
 	render() {
 
 		return (
 			<div className="content__container">
-				Content
+				<h1>{this.state.pagename}</h1>
+				<hr/>
+
+				<input 
+					type="text" 
+					placeholder="Type the text..." 
+					onChange={this.changeTextHandler}
+
+					/* Первый способ - устаревший */
+					/* ref={(inputEl) => {this.inputElement = inputEl}} */
+
+					/* 1) Второй способ - современный */
+					ref={this.inputElementRef}
+				/>
 			</div>
 	  );
 	}
