5c4f89a7ea0e288be3dc271dcde3f6b1129cc32f
Mak-Pro <maksprocode@gmail.com>
2020-08-08 15:36:39 +0300

    setState currectly

--- src/App.js
+++ src/App.js
@@ -10,11 +10,49 @@ class App extends Component {
 		super(props);
 	}
 
+	state = {
+		counter: 0,
+	}
+
+	incrementCounter = () => {
+		/*this.setState({
+			counter: this.state.counter + 1,
+		});*/
+		// Новый вариант setState (более надежный)
+		this.setState((prevState, props) => {
+			return {
+				counter: prevState.counter + 1,
+			}
+		});
+	}
+
+	decrementCounter = () => {
+		if(this.state.counter > 0) {
+			/*this.setState({
+				counter: this.state.counter - 1,
+			});*/
+
+			// Новый вариант setState (более надежный)
+			this.setState((prevState, props) => {
+				return {
+					counter: prevState.counter - 1,
+				}
+			});
+		}
+		else {
+			return null;
+		}
+	}
+
 	render() {
 
 		return (
 			<div className="content__container">
-				Content
+				<div className="counter__actions">
+					<button onClick={this.decrementCounter}>-</button>
+					<span>{this.state.counter}</span>
+					<button onClick={this.incrementCounter}>+</button>
+				</div>
 			</div>
 	  );
 	}
